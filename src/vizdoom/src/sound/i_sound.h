cdc33bbde09d7c4665b74534f740f8d102a38d51
khegde <kshegde6@gmail.com>
2020-10-22 12:45:35 -0700

    try4

--- src/vizdoom/src/sound/i_sound.h
+++ src/vizdoom/src/sound/i_sound.h
@@ -147,8 +147,8 @@ public:
 
 	virtual void UpdateListener (SoundListener *) = 0;
 	virtual void UpdateSounds () = 0;
-	virtual void UpdateMusic() {}
-    virtual void getrenderbuffer(short pInt[][2], int buf_len) {}
+	virtual void UpdateMusic() {};
+	virtual void getrenderbuffer(short pInt[][2], int buf_len) {}
 
 	virtual bool IsValid () = 0;
 	virtual void PrintStatus () = 0;
